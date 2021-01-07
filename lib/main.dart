9ce83787f640e2db84b2d4ba5101454542d2522c
treasureBear94 <qvo7896@gmail.com>
2021-01-07 22:36:15 +0900

    :sparkles: fluttertoast 를 이용한 toast메시지 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Snack bar',
+      title: 'Toast message',
       theme: ThemeData(primarySwatch: Colors.red),
       home: MyPage(),
     );
@@ -14,6 +15,36 @@ class MyApp extends StatelessWidget {
 }
 
 class MyPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Snack bar'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: FlatButton(
+            onPressed: () {
+              flutterToast();
+            },
+            child: Text('Toast'),
+            color: Colors.blue,
+          ),
+        ));
+  }
+}
+
+void flutterToast() {
+  Fluttertoast.showToast(
+      msg: 'Flutter',
+      gravity: ToastGravity.BOTTOM,
+      backgroundColor: Colors.red,
+      fontSize: 20.0,
+      textColor: Colors.white,
+      toastLength: Toast.LENGTH_SHORT);
+}
+
+class MyPageWithClass extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
